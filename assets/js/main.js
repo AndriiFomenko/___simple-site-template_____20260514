1129d1d4ae7eb9f8f41c551635029f59e6883503
Alecto <fomenkoandrey1978@gmail.com>
2026-05-21 21:13:25 +0300

    feat: initialize simple site template structure with SCSS modules and Gulp build pipeline

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-  $('.navbar__btn').click(function () {
-    $(this).toggleClass('active')
-    $('.navbar__list').toggleClass('active')
+  $(".navbar__button").click(function () {
+    $(this).toggleClass("active")
+    $(".navbar__list").toggleClass("active")
   })
 })
--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
-import { series, src, dest, watch } from 'gulp'
-import gulpSass from 'gulp-sass'
-import * as sass from 'sass'
-import replace from 'gulp-replace'
-import dc from 'postcss-discard-comments'
-import browserSync from 'browser-sync'
-import postcss from 'gulp-postcss'
-import csscomb from 'gulp-csscomb'
-import cssnano from 'cssnano'
-import rename from 'gulp-rename'
-import autoprefixer from 'autoprefixer'
-import sortMediaQueries from 'postcss-sort-media-queries'
-import pug from 'gulp-pug'
-import fs from 'fs'
+import { series, src, dest, watch } from "gulp"
+import gulpSass from "gulp-sass"
+import * as sass from "sass"
+import replace from "gulp-replace"
+import dc from "postcss-discard-comments"
+import browserSync from "browser-sync"
+import postcss from "gulp-postcss"
+import csscomb from "gulp-csscomb"
+import cssnano from "cssnano"
+import rename from "gulp-rename"
+import autoprefixer from "autoprefixer"
+import sortMediaQueries from "postcss-sort-media-queries"
+import pug from "gulp-pug"
+import fs from "fs"
 
 // Динамічний імпорт для sort-css-media-queries
-const { default: sortCSSmq } = await import('sort-css-media-queries')
+const { default: sortCSSmq } = await import("sort-css-media-queries")
 
 // Налаштування Sass компілятора
 const sassCompiler = gulpSass(sass)
@@ -22,30 +22,30 @@ const sassCompiler = gulpSass(sass)
 const option = process.argv[3]
 
 const PATH = {
-  scssFolder: './src/scss/',
-  scssAllFiles: ['./src/scss/**/*.scss', '!**/_mixins-media.scss', '!**/_variables.scss', '!**/_skins.scss'],
-  scssRootFile: './src/scss/style.scss',
-  pugFolder: './src/templates/',
-  pugAllFiles: './src/templates/**/*.pug',
-  pugRootFile: './src/templates/index.pug',
-  cssFolder: './assets/css/',
-  cssAllFiles: './assets/css/*.css',
-  cssRootFile: './assets/css/style.css',
-  htmlFolder: './',
-  htmlAllFiles: './*.html',
-  jsFolder: './assets/js/',
-  jsAllFiles: './assets/js/**/*.js',
-  imagesFolder: './assets/images/',
-  vendorsFolder: './assets/vendors/'
+  scssFolder: "./src/scss/",
+  scssAllFiles: ["./src/scss/**/*.scss", "!**/_mixins-media.scss", "!**/_variables.scss", "!**/_skins.scss"],
+  scssRootFile: "./src/scss/style.scss",
+  pugFolder: "./src/templates/",
+  pugAllFiles: "./src/templates/**/*.pug",
+  pugRootFile: "./src/templates/index.pug",
+  cssFolder: "./assets/css/",
+  cssAllFiles: "./assets/css/*.css",
+  cssRootFile: "./assets/css/style.css",
+  htmlFolder: "./",
+  htmlAllFiles: "./*.html",
+  jsFolder: "./assets/js/",
+  jsAllFiles: "./assets/js/**/*.js",
+  imagesFolder: "./assets/images/",
+  vendorsFolder: "./assets/vendors/"
 }
 
 const SEARCH_IMAGE_REGEXP = /url\(['"]?.*\/images\/(.*?)\.(png|jpe?g|gif|webp|svg)['"]?\)/g
-const REPLACEMENT_IMAGE_PATH = 'url(../images/$1.$2)'
+const REPLACEMENT_IMAGE_PATH = "url(../images/$1.$2)"
 
 const PLUGINS = [
   dc({ discardComments: true }),
   autoprefixer({
-    overrideBrowserslist: ['last 5 versions', '> 0.1%']
+    overrideBrowserslist: ["last 5 versions", "> 0.1%"]
   }),
   sortMediaQueries({
     sort: sortCSSmq
@@ -54,7 +54,7 @@ const PLUGINS = [
 
 function compileScss() {
   return src(PATH.scssRootFile)
-    .pipe(sassCompiler().on('error', sassCompiler.logError))
+    .pipe(sassCompiler().on("error", sassCompiler.logError))
     .pipe(postcss(PLUGINS))
     .pipe(replace(SEARCH_IMAGE_REGEXP, REPLACEMENT_IMAGE_PATH))
     .pipe(dest(PATH.cssFolder))
@@ -62,13 +62,13 @@ function compileScss() {
 }
 
 function compileScssMin() {
-  const pluginsForMinify = [...PLUGINS, cssnano({ preset: 'default' })]
+  const pluginsForMinify = [...PLUGINS, cssnano({ preset: "default" })]
 
   return src(PATH.scssRootFile)
-    .pipe(sassCompiler().on('error', sassCompiler.logError))
+    .pipe(sassCompiler().on("error", sassCompiler.logError))
     .pipe(replace(SEARCH_IMAGE_REGEXP, REPLACEMENT_IMAGE_PATH))
     .pipe(postcss(pluginsForMinify))
-    .pipe(rename({ suffix: '.min' }))
+    .pipe(rename({ suffix: ".min" }))
     .pipe(dest(PATH.cssFolder))
 }
 
@@ -78,7 +78,7 @@ function compileScssDev() {
   pluginsForDevMode.splice(1, 1)
 
   return src(PATH.scssRootFile, { sourcemaps: true })
-    .pipe(sassCompiler().on('error', sassCompiler.logError))
+    .pipe(sassCompiler().on("error", sassCompiler.logError))
     .pipe(postcss(pluginsForDevMode))
     .pipe(replace(SEARCH_IMAGE_REGEXP, REPLACEMENT_IMAGE_PATH))
     .pipe(dest(PATH.cssFolder, { sourcemaps: true }))
@@ -97,7 +97,7 @@ function comb() {
 
 function serverInit() {
   browserSync({
-    server: { baseDir: './' },
+    server: { baseDir: "./" },
     notify: false
   })
 }
@@ -109,8 +109,8 @@ async function sync() {
 function watchFiles() {
   serverInit()
   if (!option) watch(PATH.scssAllFiles, series(compileScss, compileScssMin))
-  if (option === '--dev') watch(PATH.scssAllFiles, series(compileScssDev))
-  if (option === '--css') watch(PATH.cssAllFiles, sync)
+  if (option === "--dev") watch(PATH.scssAllFiles, series(compileScssDev))
+  if (option === "--css") watch(PATH.cssAllFiles, sync)
   watch(PATH.htmlAllFiles, sync)
   watch(PATH.pugAllFiles, series(compilePug, sync))
   watch(PATH.jsAllFiles, sync)
@@ -120,32 +120,32 @@ function createStructure() {
   // Структура SCSS файлів по папках за патерном 7-1
   const scssFiles = {
     abstracts: [
-      '_index',
-      '_variables', // змінні проекту
-      '_skin', // кольори, тіні, градієнти
-      '_mixins', // міксини
-      '_mixins-media', // міксини для медіа-запитів
-      '_extends' // плейсхолдери
+      "_index",
+      "_variables", // змінні проекту
+      "_skin", // кольори, тіні, градієнти
+      "_mixins", // міксини
+      "_mixins-media", // міксини для медіа-запитів
+      "_extends" // плейсхолдери
     ],
     base: [
-      '_index',
-      '_common', // базові стилі
-      '_typography' // типографіка
+      "_index",
+      "_common", // базові стилі
+      "_typography" // типографіка
     ],
-    layout: ['_index', '_header', '_footer', '_main'],
+    layout: ["_index", "_header", "_footer", "_main"],
     components: [
-      '_index'
+      "_index"
       // тут будуть компоненти
     ],
     root: [
-      'style' // головний файл
+      "style" // головний файл
     ]
   }
 
   // Створюємо масив шляхів для всіх SCSS файлів
   const scssAllFiles = Object.entries(scssFiles).flatMap(([folder, files]) => {
     return files.map((fileName) =>
-      folder === 'root' ? `${PATH.scssFolder}${fileName}.scss` : `${PATH.scssFolder}${folder}/${fileName}.scss`
+      folder === "root" ? `${PATH.scssFolder}${fileName}.scss` : `${PATH.scssFolder}${folder}/${fileName}.scss`
     )
   })
 
@@ -158,13 +158,13 @@ function createStructure() {
   ]
 
   // Створюємо папки для SCSS
-  const scssFolders = ['abstracts', 'base', 'layout', 'components']
+  const scssFolders = ["abstracts", "base", "layout", "components"]
   scssFolders.forEach((folder) => {
     fs.mkdirSync(`${PATH.scssFolder}${folder}`, { recursive: true })
   })
 
   // Створюємо основні папки проекту
-  src('*.*', { read: false })
+  src("*.*", { read: false })
     .pipe(dest(PATH.scssFolder))
     .pipe(dest(PATH.pugFolder))
     .pipe(dest(PATH.cssFolder))
@@ -178,19 +178,19 @@ function createStructure() {
         if (Array.isArray(filePath)) {
           filePath.forEach((subPath) => {
             // Створюємо папку, якщо її немає
-            const dir = subPath.substring(0, subPath.lastIndexOf('/'))
+            const dir = subPath.substring(0, subPath.lastIndexOf("/"))
             if (!fs.existsSync(dir)) {
               fs.mkdirSync(dir, { recursive: true })
             }
-            fs.writeFileSync(subPath, '')
+            fs.writeFileSync(subPath, "")
             console.log(subPath)
           })
         } else {
-          const dir = filePath.substring(0, filePath.lastIndexOf('/'))
+          const dir = filePath.substring(0, filePath.lastIndexOf("/"))
           if (!fs.existsSync(dir)) {
             fs.mkdirSync(dir, { recursive: true })
           }
-          fs.writeFileSync(filePath, '')
+          fs.writeFileSync(filePath, "")
           console.log(filePath)
         }
       })
